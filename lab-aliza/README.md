0c51782e73ce5c97ce3179c14a1f0932b716cf46
Aliza Pilisuk <apilisuk@gmail.com>
2016-08-20 14:24:08 -0700

    initial set up from class demo and previous labs

---
+++ lab-aliza/README.md
@@ -0,0 +1,1 @@
+## Aliza's Angular Controller CRUD Lab
---
+++ lab-aliza/app/controller/crud-controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const angular = require('angular');
+const crudApp = angular.module('crudApp');
+
+crudApp.controller('ListController', ['$log', '$http', ListController]);
+
+function ListController($log, $http){
+  this.lists = [];
+  let baseUrl = `${__API_URL__}/api/list`;
+  let config = {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  };
+
+  this.createList = function(list){
+    $log.debug('listCtrl.createList');
+    $http.post(baseUrl, list, config)
+      .then(res => {
+        $log.log('Success!', res.data);
+        this.lists.push(res.data);
+      })
+      .catch(err => {
+        $log.log('Errororororor', err);
+      });
+  };
+}
