81acabd0e0e1d3311905741fcba2a1c6b41e38cd
chops318 <chops318@gmail.com>
2016-08-22 12:09:24 -0700

    added gitignore and theme

--- lab-edward-smith/test/list-controller-test.js
+++ lab-edward-smith/test/list-controller-test.js
@@ -9,18 +9,37 @@ describe('testing list controller', function() {
     });
   });
 
-  it('should create a list', () => {
-    let url = 'http://localhost:3000/api/list';
-    let requestData = {name: 'Chops'};
-    this.$httpBackend.expectPost(url, requestData)
+  beforeEach(() => {
+    this.url = 'http://localhost:3000/api/list/';
+    this.requestData = {name: 'Chops'};
+    this.$httpBackend.expectPOST(this.url, this.requestData)
       .respond(200, {
         name: 'Chops',
-        _id: '1234'
+        _id: '1234',
         __v: 0,
         notes: []
       });
-
-      this.listCtrl.createList(requestData);
-      this.$httpBackend.flush();
   })
+
+  it('should create a list', () => {
+      this.listCtrl.createList(this.requestData);
+      this.$httpBackend.flush();
+      expect(this.listCtrl.lists.length).toBe(1)
+  })
+
+  it('should get the list of notes', () => {
+    this.$httpBackend.expectGET(this.url)
+      .respond(200)
+    this.listCtrl.getList();
+    console.log(this.listCtrl.lists)
+  })
+
+  // it('should delete an item from the list', () => {
+  //   this.$httpBackend.expectDELETE(this.url)
+  //     .respond(200);
+  //   this.listCtrl.createList(this.requestData);
+  //   this.listCtrl.deleteListItem('1234');
+  //   this.$httpBackend.flush();
+  //   expect(this.listCtrl.lists).toBe(0)
+  // })
 })
