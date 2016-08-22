44c602cddd7ae7c33106786c944b800ed44bfdd2
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-08-21 21:28:54 -0700

    added crud routes in list_controller.js to create, read, and delete

--- lab-james/app/entry.js
+++ lab-james/app/entry.js
@@ -4,4 +4,6 @@ require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/base.scss');
 
 const angular = require('angular');
-angular.module('imageApp', []);
+angular.module('listApp', []);
+
+require('./controller/list_controller');
