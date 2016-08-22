4487980635cd9a496e13ade5ff688662e3efeaa8
Jeffrey Gebhardt <jeff@gebbey.com>
2016-08-22 12:24:19 -0700

    added more logic

--- lab-jeff/app/controller/list-controller.js
+++ lab-jeff/app/controller/list-controller.js
@@ -3,14 +3,14 @@
 const angular = require('angular');
 const crudyApp = angular.module('crudyApp');
 
-crudyApp.controller('ListController', ['$http', ListController]);
+crudyApp.controller('ListController', ['$log', '$http', ListController]);
 
-function ListController($http) {
+function ListController($log, $http) {
   let lc = this;
 
   lc.lists = [];
 
-  let baseUrl = `${__API_URL__}api/list`;
+  let baseUrl = `${__API_URL__}/api/list/`;
   let config = {
     headers: {
       'Accept': 'application/json',
@@ -18,10 +18,49 @@ function ListController($http) {
     },
   };
 
+  lc.getAllLists = function() {
+    $http.get(baseUrl, config)
+      .then((res) => {
+        $log.log('Success!', res.data);
+        res.data.forEach((list) => {
+          lc.lists.push(list);
+        });
+      })
+      .catch((err) => {
+        return err;
+      });
+  };
+
+  lc.deleteAllLists = function() {
+    $http.delete(baseUrl, config)
+      .then((res) => {
+        $log.log('Success!', res.data);
+        lc.lists = [];
+      })
+      .catch((err) => {
+        return err;
+      });
+  };
+
   lc.createList = function(listName) {
     $http.post(baseUrl, listName, config)
       .then((res) => {
+        $log.log('Success!', res.data);
         lc.lists.push(res.data);
+      })
+      .catch((err) => {
+        return err;
+      });
+  };
+
+  lc.deleteList = function(listId) {
+    $http.delete(baseUrl + listId, config)
+      .then((res) => {
+        $log.log('Success', res.data);
+        lc.lists = lc.lists.filter((list) => {
+          return list._id !== listId;
+        });
+        console.log(lc.lists);
       })
       .catch((err) => {
         return err;
--- lab-jeff/app/entry.js
+++ lab-jeff/app/entry.js
@@ -4,6 +4,6 @@ require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/base.scss');
 
 const angular = require('angular');
-angular.module('ListController', []);
+angular.module('crudyApp', []);
 
 require('./controller/list-controller.js');
