81ddfcd9105f2d3cdd65bd4c439106d9ca30155b
Mars <marshallmiller92@gmail.com>
2016-08-22 12:15:21 -0700

    updated httpbackend testing

--- lab-Marshall/test/controller-test.js
+++ lab-Marshall/test/controller-test.js
@@ -3,8 +3,9 @@
 describe('testing controller', function(){
   beforeEach(() => {
     angular.mock.module('demoApp');
-    angular.mock.inject(($controller) => {
+    angular.mock.inject(($controller, $httpBackend) => {
       this.gameCtrl = $controller('GameController');
+      this.$httpBackend = $httpBackend;
     });
   });
 
@@ -29,5 +30,24 @@ describe('testing controller', function(){
   it('should create a new monster on submit', () => {
     this.monsterCtrl.submit({name: 'Ultra Lord'});
     expect(this.monsterCtrl.monster.name).toBe('Ultra Lord');
+  });
+
+  it('should create a list', () => {
+    let url = 'http://localhost:3000/api/list';
+    let requestData = {name: 'example name'};
+    let headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+    this.$httpBackend.expectPOST(url, requestData, headers)
+    .respond(200, {
+      name: 'test name',
+      _id: 'dfliisdjflsdhf',
+      __v: 0,
+      notes: []
+    });
+
+    this.listCtrl.createList(requestData);
+    this.$httpBackend.flush();
   });
 });
