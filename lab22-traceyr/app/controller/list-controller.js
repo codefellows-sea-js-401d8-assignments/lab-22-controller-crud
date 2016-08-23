169e46cf5fa8b45da8674f8c323f1ec2e3a76eb3
Tracey <radcliffe.tracey@gmail.com>
2016-08-22 19:32:55 -0700

    caught up with class

--- lab22-traceyr/app/controller/list-controller.js
+++ lab22-traceyr/app/controller/list-controller.js
@@ -10,7 +10,20 @@ function ListController($log, $http){
   let baseUrl = `${__API_URL__}/api/list`;
   let config = {
     headers: {
-      'Accept': 'application/json', 
+      'Accept': 'application/json',
     }
+  };
+
+  this.createList = function(list){
+    $log.debug('listCtrl.createList');
+    $http.post(baseUrl, list, config)
+      .then( (res)=>{
+        $log.log('success!', res.data);
+        this.lists.push(res.data);
+      })
+      .catch((err)=>{
+        $log.error('error!', err);
+        alert('EHHHHHHHHHHH');
+      });
   }
 }
