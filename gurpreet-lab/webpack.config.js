688b60b5db45e55a878d29692e7ea57df4c5826d
preetsohal <preetsohal@live.com>
2016-08-22 20:36:42 -0700

    very close

--- gurpreet-lab/webpack.config.js
+++ gurpreet-lab/webpack.config.js
@@ -1,51 +1,53 @@
 'use strict';
+
 const webpack = require('webpack');
 const ExtractText = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
-const API_URL = JSON.stringyfy(process.env.API_URL || 'http://localhost:3000');
+const API_URL = JSON.stringify(process.env.API_URL || 'http://localhost:3000');
+
 let plugins = [
   new ExtractText('bundle.css'),
   new webpack.DefinePlugin({
-    __API_URL__: API_URL,
-  });
+    __API_URL__: API_URL
+  })
 ];
+
 module.exports = {
-  entry:`${__dirname}/app/entry.js`,
+  entry: `${__dirname}/app/entry.js`,
   plugins: plugins,
   output: {
     path: 'build',
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
-  potcss: function(){
+  postcss: function(){
     return [autoprefixer];
-  }
-};
-sassLoader: {
-  includePaths:[`${__dirname}/app/scss/lib`]
-},
-module: {
-  loaders: [
-    {
-      test: /\.scss$/,
-      loader: ExtractText.extract('style', 'css!postcss!sass!'),
-    },
-    {
-      test: /\.js$/,
-      loader: 'babel',
-      exclude: /node_modules/,
-      query: {
-        presets: ['es2015'],
+  },
+  sassLoader: {
+    include: [`${__dirname}/app/scss/lib`]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractText.extract('style', 'css!postcss!sass!')
       },
-    },
-    {
-      test: /\.(jpg|gif)$/,
-      loader: 'file?name=image/[hash]-[name].[ext]',
-    },
-    {
-      test: /\.(woff|svg|eof|ttf).*/,
-      loader: 'url?limit=10000&name=font/[name].[ext]',
-    },
-  ],
-},
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.(jpg|gif)$/,
+        loader: 'file?name=img/[hash]-[name].[ext]'
+      },
+      {
+        test: /\.(woff|svg|eot|ttf).*/,
+        loader: 'url?limit=10000&name=font/[name].[ext]'
+      }
+    ]
+  }
 };
