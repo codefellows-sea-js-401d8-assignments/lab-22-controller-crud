344c24263a1596408023a9389821f81b4c4f3709
Tracey <radcliffe.tracey@gmail.com>
2016-08-31 11:20:26 -0700

    initial test working

--- lab22-traceyr/karma.conf.js
+++ lab22-traceyr/karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function(config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
+    webpack: webpackConfig,
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
--- lab22-traceyr/test/list-controller-test.js
+++ lab22-traceyr/test/list-controller-test.js
@@ -1,1 +1,30 @@
 'use strict';
+
+describe('testing list-controller', function(){
+
+  beforeEach(() => {
+    angular.mock.module('lab22');
+    angular.mock.inject(($controller, $httpBackend) => {
+      this.listCtrl = new $controller('ListController');
+      this.$httpBackend = $httpBackend;
+    });
+  });
+
+  it('should test creating a list', ()=>{
+    let url = 'http://localhost:3000/api/list';
+    let requestData = {name: 'testing name'};
+    let headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    };
+    this.$httpBackend.expectPOST(url, requestData, headers)
+    .respond(200, {
+      name: 'testing name',
+      _id: '1111',
+      __v: 0,
+      notes: []
+    });
+    this.listCtrl.createList(requestData);
+    this.$httpBackend.flush();
+  });
+});
